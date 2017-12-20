3e140249a6405fe8dea30484b44ad8c0d5ec304f
Treyhboy <taruntrehen@gmail.com>
2017-12-20 07:31:36 +0530

    recent change

--- src/Components/HomePage/MyContent.js
+++ src/Components/HomePage/MyContent.js
@@ -8,7 +8,7 @@ class MyContent extends React.Component {
 
     render()
     {
-        return ( <Content style={{ minHeight:'2000px',backgroundColor:"#fff",marginTop:"70px" }}>
+        return ( <Content style={{ minHeight:'2000px',backgroundColor:"#fff",marginTop:"90px" }}>
             <Row  type="flex" justify="space-between">
             <Col span={6} style={{backgroundColor:"#E9EBEF",minHeight:"2000px"}}/>
             <Col span={10}><Post name="post 1"/><Post name="post 2"/><Post name="post 3"/></Col>
